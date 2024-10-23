055da2d8527e5856cc441a09ab9c7403d5c50c3d
Your Name <you@example.com>
2024-10-23 18:29:59 +0200

    boletin.up

---
+++ programacion/funciones/ejercicio1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void lineasblanco(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.println("\n");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println();
+        System.out.println();
+        System.out.println(" ¿Cuántas líneas en blanco quieres poner?");
+        n = sc.nextInt();
+        lineasblanco(n);
+
+
+
+    }
+
+}
+
