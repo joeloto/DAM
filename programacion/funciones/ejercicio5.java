cb4d6a1072bce5810939c1b601e6141b905d78ba
Your Name <you@example.com>
2024-10-30 13:57:17 +0100

    act

--- programacion/funciones/ejercicio5.java
+++ programacion/funciones/ejercicio5.java
@@ -1,3 +1,20 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class ejercicio5 {
+
+    public static boolean bisiesto(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("Dime un año");
+        year = sc.nextInt();
+
+        PrintWriter f = new PrintWriter("ejercicio5.txt");
+    }
+    
     
 }
