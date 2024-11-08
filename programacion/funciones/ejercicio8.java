032be958202a1406e0e0b9c8c423a1b3259170f4
Your Name <you@example.com>
2024-11-08 12:51:12 +0100

    act.primo

--- programacion/funciones/ejercicio8.java
+++ programacion/funciones/ejercicio8.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class ejercicio8 {
-    
-    public static boolean primo(int num){
-        if (num == 1){
+
+    public static boolean primo(int num) {
+        if (num == 1) {
             return false;
         }
-        
+
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
@@ -14,22 +14,27 @@ public class ejercicio8 {
         }
         return true;
     }
-    
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dime un número");
+        // System.out.println("Dime un número");
         int num;
-        //System.out.println(primo(num));
-        
-        do{
+        // System.out.println(primo(num));
+
+        do {
             System.out.println("Dime un valor");
             num = sc.nextInt();
             if (num < 2) {
                 System.out.println("Dame un valor mayor que 2");
             }
-        }while (num < 2);
+        } while (num < 2);
+
+        for (int cont = num; cont >= 2; cont--) {
+            if (primo(cont) == true) {
+                if (cont != num) {
+                    System.out.println(cont);
+                }
+            }
+        }
     }
 }
-
-    
