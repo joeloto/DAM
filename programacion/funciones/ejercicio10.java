9bb5342e120dee43bc4c349be6aaa7a8401cf79f
Your Name <you@example.com>
2024-11-11 11:17:10 +0100

    act.ejer10

--- programacion/funciones/ejercicio10.java
+++ programacion/funciones/ejercicio10.java
@@ -18,6 +18,8 @@ public class ejercicio10 {
         int base;
         int altura;
         int opc;
+        int lado;
+        int radio;
         do {
             System.out.println("-----------------------------------------------");
             System.out.println("Pulsa 1 para calcular la base de un rectángulo");
@@ -26,35 +28,68 @@ public class ejercicio10 {
             System.out.println("-----------------------------------------------");
             System.out.println("Pulsa 3 para calcular la base de un cuadrado");
             System.out.println("-----------------------------------------------");
-            System.out.println("Pulsa 4 para calcular la base de un cículo");
+            System.out.println("Pulsa 4 para calcular la base de un círculo");
             System.out.println("-----------------------------------------------");
             opc = sc.nextInt();
             switch (opc) {
                 case 1:
+                    System.out.println("Has seleccionado hacer el área de un rectángulo");
                     do {
                         System.out.println("Dame un valor para la base");
                         base = sc.nextInt();
                         System.out.println("Dame un valor para la altura");
                         altura = sc.nextInt();
                         if (altura < 0 || base < 0) {
-                            System.out.println("Dame un valor positivo");
+                            System.out.println("Dame valores positivo");
                         }
                     } while (base < 0 || altura < 0);
                     System.out.printf("El área del rectángulo es %.2f", area(true, base, altura));
+                    System.out.println();
                     break;
                 case 2:
+                    System.out.println("Has seleccionado hacer el área de un triángulo");
+                    do {
+                        System.out.println("Dame un valor para la base");
+                        base = sc.nextInt();
+                        System.out.println("Dame un valor para la altura");
+                        altura = sc.nextInt();
+                        if (altura < 0 || base < 0) {
+                            System.out.println("Dame valores positivos");
+                        }
+                    } while (base < 0 || altura < 0);
+                    System.out.printf("El área del rectángulo es %.2f", area(false, base, altura));
+                    System.out.println();
                     break;
                 case 3:
+                    System.out.println("Has seleccionado hacer el área de un cuadrado");
+                    do {
+                        System.out.println("Dame un valor para los lados");
+                        lado = sc.nextInt();
+                        if (lado < 0) {
+                            System.out.println("Dame valores positivos");
+                        }
+                    } while (lado < 0);
+                    System.out.printf("El área del cuadrado es %.2f", area(true, lado, lado));
+                    System.out.println();
                     break;
                 case 4:
+                    System.out.println("Has seleccionado hacer el área de un círculo");
+                    do {
+                        System.out.println("Dame un valor para el radio");
+                        radio = sc.nextInt();
+                        if (radio < 0) {
+                            System.out.println("Dame un valor positivo");
+                        }
+                    } while (radio < 0);
+                    System.out.printf("El área del círculo es %.2f", Math.PI * (Math.pow(radio, 2)));
+                    System.out.println();
                     break;
-
                 default:
+                    System.out.println("Escoge una opción de las indicadas");
+                    System.out.println();
                     break;
             }
         } while (opc != 4);
-        // System.out.printf("El área del rectángulo es %.2f.", area(true, base,
-        // altura));
 
     }
 
--- programacion/funciones/ejercicio7.java
+++ programacion/funciones/ejercicio7.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class ejercicio7 {
 
-    public static loadFile(String name){
-        String a = name;
-        Scanner f = new Scanner(new File (name));
-        a = f.nextLine();
-        f.close();
-        return a;
-    };
+    // public static loadFile(String name){
+    //     String a = name;
+    //     Scanner f = new Scanner(new File (name));
+    //     a = f.nextLine();
+    //     f.close();
+    //     return a;
+    // };
 
 }
