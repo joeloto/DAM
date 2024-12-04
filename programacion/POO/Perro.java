358de06fdfc4ba11b54b8268d5071e123c3d1db0
Your Name <you@example.com>
2024-12-04 13:56:10 +0100

    poo2

--- programacion/POO/Perro.java
+++ programacion/POO/Perro.java
@@ -9,7 +9,34 @@ public class Perro {
         return edad;
     }
 
-    public void setEdad(int a) {
-        edad = a;
+    public void setEdad(int edad) {
+        if (edad > 0) {
+            this.edad = edad;
+        } else{
+            this.edad = 0;
+        }
+    }
+
+    private double temperatura;
+
+    public void setTemperatura(double temperatura){
+        this.temperatura = temperatura;
+        if (temperatura > 39) {
+            fiebre = true;
+        } else{
+            fiebre = false;
+        }
+    }
+
+    private boolean fiebre;
+    public boolean getFiebre(){
+        return fiebre;
+    }
+
+    public Perro(String nombre, String raza, int edad, double temperatura){
+        this.nombre = nombre;
+        this.raza = raza;
+        this.setEdad(edad);
+        this.setTemperatura(temperatura);
     }
 }
--- programacion/POO/Principal.java
+++ programacion/POO/Principal.java
@@ -3,13 +3,9 @@ package programacion.POO;
 public class Principal {
 
     public static void main(String[] args) {
-        Perro perro = new Perro();
+        Perro perro = new Perro("Skipper","can palleiro",5,36);
 
-        perro.nombre = "Skipper";
-
-        perro.raza = "can palleiro";
-
-        perro.setEdad(4);
+        // perro.fiebre = false;
 
         System.out.printf("Tengo un %s llamado %s de %d años\n",
                 perro.raza, perro.nombre,
