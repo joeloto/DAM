6c3fd386cbe196792f20d22063c82a40498e574c
Your Name <you@example.com>
2025-01-30 13:56:16 +0100

    hyestr

--- programacion/arrays/boletin/Ventas.java
+++ programacion/arrays/boletin/Ventas.java
@@ -1,6 +1,54 @@
 package programacion.arrays.boletin;
 
+import java.time.LocalDate;
+
 public class Ventas {
     public int[] vector = new int[12];
     private int year;
+    LocalDate fechaActual = LocalDate.now();
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year >= fechaActual.getYear()) {
+            this.year = year - 1;
+        } else {
+            this.year = year;
+        }
+    }
+
+    public Ventas(int year) {
+        for (int i = 0; i < this.vector.length; i++) {
+            this.vector[i] = (int) (Math.random() * 1000);
+        }
+        setYear(year);
+    }
+
+    public Ventas(int year, int[] vector) {
+        setYear(year);
+        this.year = year;
+        if (vector.length != 12) {
+            int[] vector2 = new int[12];
+            for (int i = 0; i < vector2.length; i++) {
+                vector2[i] = (int) (Math.random() * 1000);
+            }
+            this.vector = vector2;
+        }
+    }
+
+    public double media() {
+        int acumulador = 0;
+        int cont;
+        for (cont = 0; cont < vector.length; cont++) {
+            acumulador += vector[cont];
+        }
+        double media = acumulador / cont;
+        return media;
+    }
+
+    public void grafica(){
+
+    }
 }
--- programacion/arrays/boletin/ejercicio4.java
+++ programacion/arrays/boletin/ejercicio4.java
@@ -2,25 +2,41 @@ package programacion.arrays.boletin;
 
 import java.util.*;
 
-public class ejercicio4 {
+public class ejercicio4 {//TODO comentarios
 
+    /**
+     * muestra cada letra de una cadena en una linea distinta
+     * @param cadena recoge la cadena de la que se extraen las letras
+     */
     public static void muestraEnLinea(String cadena) {
         for (int i = 0; i < cadena.length(); i++) {
             System.out.println(cadena.charAt(i));
         }
     }
 
+    /**
+     * replica del substring
+     * @param cadena recoge la cadena que se quiere utilizar
+     * @param posicionInicio recoge la posicion de inicio de la cadena desde donde se quiere empezar
+     * @param numCaracteres recoge el numero de caracteres que se quieren coger
+     * @return devuelve la cadena resultante o una cadena vacia si no se cumple el if
+     */
     public static String subCadena(String cadena, int posicionInicio, int numCaracteres) {
         String cadenaDefinitiva = "";
+        if (cadena == null || numCaracteres > cadena.length() || posicionInicio > cadena.length()) {//TODO apliar compro
+            return "";
+        }
         for (int i = posicionInicio; i < posicionInicio + numCaracteres; i++) {
             cadenaDefinitiva += cadena.charAt(i);
         }
-        if (cadena == null || numCaracteres > cadena.length()) {
-            return "";
-        }
+        
         return cadenaDefinitiva;
     }
 
+    /**
+     * centra una cadena en la consola
+     * @param cadena recoge la cadena que se quiere centrar
+     */
     public static void muestraCentrado(String cadena) {
         int consola = 80;
         int espacios = (consola - cadena.length())/2;
@@ -30,6 +46,11 @@ public class ejercicio4 {
         System.out.print(cadena);
     }
 
+    /**
+     * elabora un vector de char con una cadena
+     * @param cadena recoge la cadena de la que se extraen los char
+     * @return devuelve el vector
+     */
     public static char[] cadenaAVector(String cadena) {
         char[] vector = new char[cadena.length()];
         for (int i = 0; i < cadena.length(); i++) {
@@ -38,6 +59,11 @@ public class ejercicio4 {
         return vector;
     }
 
+    /**
+     * imprime una cadena al reves
+     * @param cadena recoge la cadena que se quiere poner al reves
+     * @return devuelve la cadena al reves
+     */
     public static String alReves(String cadena) {
         String reves = "";
         for (int i = cadena.length() - 1; i >= 0; i--) {
@@ -46,17 +72,24 @@ public class ejercicio4 {
         return reves;
     }
 
+    /**
+     * une las cadenas y las pone en mayusculas y si hay _ lo sustituye or un guion bajo
+     * @param numeroCadenas recoge las cadenas (numero indeterminado)
+     * @return devuelve la cadena resultante
+     */
     public static String pasoAMayusculas(String... numeroCadenas) {
         String cadenas = "";
         String cadena2 = "";
         for (int i = 0; i < numeroCadenas.length; i++) {
-            cadenas += numeroCadenas[i];
-            for (int j = 0; j < cadenas.length(); j++) {
+            cadenas = numeroCadenas[i];
+            for (int j = 0; j < cadenas.length(); j++) {//TODO completar
                 if (cadenas.charAt(j) >= 'a' && cadenas.charAt(j) <= 'z') {
                     cadena2 += (char) (cadenas.charAt(j) - 32);
                 }
                 if (cadenas.charAt(j) == '_') {
                     cadena2 += " ";
+                } else{
+                    cadena2 += cadenas;
                 }
             }
         }
@@ -68,13 +101,13 @@ public class ejercicio4 {
 
         // System.out.println( (char)(a.charAt(0)+10) );
 
-        String b = pasoAMayusculas("bla_de", "runner");
+        String b = pasoAMayusculas("bla_de", "runner", "ABCabc","1234/()");
+        System.out.println(b);
         // muestraEnLinea(a);
-        // System.out.println(subCadena(a, 2, 4));
-        muestraCentrado(a);
-        // System.out.println(cadenaAVector(a));
+        //System.out.println(subCadena(a, 2, 49));
+        //muestraCentrado(a);
+        //System.out.println(cadenaAVector(a));
         // System.out.println(alReves(a));
-        // System.out.println(b);
 
     }
 }
