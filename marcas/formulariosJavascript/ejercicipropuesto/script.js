1f92f633692dd911a9aa2517becf5e86519d901a
Your Name <you@example.com>
2025-03-24 10:39:19 +0100

    mmamamamam

--- marcas/formulariosJavascript/ejercicipropuesto/script.js
+++ marcas/formulariosJavascript/ejercicipropuesto/script.js
@@ -1,3 +1,10 @@
 function cambio(){
-    
+    var figura = document.getElementsByName("figura");
+    if (figura.options[figura.selectedIndex].value === "Cube"){
+        document.getElementsByName("lado").style.display = "flex";
+    }
+}
+
+window.onload = function(){
+    document.onload = cambio;
 }
