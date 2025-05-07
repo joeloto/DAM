1db9c1011a4e6c03a9c20b3db50c15b9176754c5
Your Name <you@example.com>
2025-05-07 13:56:33 +0200

    swinggggg

--- programacion/boletin_swing/src/main/java/principal/Ejercicio4/Formulario.java
+++ programacion/boletin_swing/src/main/java/principal/Ejercicio4/Formulario.java
@@ -2,53 +2,120 @@ package principal.Ejercicio4;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class Formulario extends JFrame implements ActionListener{
-    
+import boletin_tema5.ejercicio4.Ejercicio4;
+
+public class Formulario extends JFrame implements ActionListener {
+
     private JLabel lblNombre;
     private JLabel lblEdad;
     private JLabel lblDir;
     private JTextField txfNombre;
     private JTextField txfEdad;
     private JTextField txfDir;
-    
+    private JButton btnGuardar;
+    private JButton btnCargar;
+
     public Formulario() {
         super("Formulario");
+        setLayout(null);
         lblNombre = new JLabel("Nombre:");
-        lblNombre.setSize(100,35);
+        lblNombre.setSize(100, 35);
         lblNombre.setVisible(true);
         this.add(lblNombre);
         txfNombre = new JTextField();
-        txfNombre.setSize(100,35);
+        txfNombre.setBounds(60, 0, 100, 35);
         txfNombre.setVisible(true);
         this.add(txfNombre);
 
         lblEdad = new JLabel("Edad:");
-        lblEdad.setSize(100,35);
+        lblEdad.setBounds(0, 50, 100, 35);
         lblEdad.setVisible(true);
         this.add(lblEdad);
         txfEdad = new JTextField();
-        txfEdad.setSize(100,35);
+        txfEdad.setBounds(60, 50, 100, 35);
         txfEdad.setVisible(true);
         this.add(txfEdad);
-        
+
         lblDir = new JLabel("Dirección:");
-        lblDir.setSize(100,35);
+        lblDir.setBounds(0, 100, 100, 35);
         lblDir.setVisible(true);
         this.add(lblDir);
         txfDir = new JTextField();
-        txfDir.setSize(100,35);
+        txfDir.setBounds(60, 100, 100, 35);
         txfDir.setVisible(true);
         this.add(txfDir);
+
+        btnGuardar = new JButton("Guardar");
+        btnGuardar.setBounds(15, 150, 80, 30);
+        btnGuardar.setVisible(true);
+        btnGuardar.addActionListener(this);
+        this.add(btnGuardar);
+
+        btnCargar = new JButton("Cargar");
+        btnCargar.setBounds(110, 150, 80, 30);
+        btnCargar.setVisible(true);
+        btnCargar.addActionListener(this);
+        this.add(btnCargar);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        if (e.getSource() == btnGuardar) {
+            int edad = Integer.parseInt(txfEdad.getText());
+            if (txfDir.getText().equals("") || txfNombre.getText().equals("") || edad < 0) {
+                JOptionPane.showMessageDialog(null, "DATOS ERRÓNEOS", "ERROR!", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                try {
+                    PrintWriter f = new PrintWriter("datos.txt");
+                    f.println("Nombre: " + txfNombre.getText());
+                    f.println("Edad: " + txfEdad.getText());
+                    f.println("Dirección: " + txfDir.getText());
+                    f.close();
+                } catch (Exception excp) {
+                    JOptionPane.showMessageDialog(null, "No se guardaron los datos", "ERROR!",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        }
+        if (e.getSource() == btnCargar) {
+            try {
+                String linea = "";
+                String linea2 = "";
+                String linea3 = "";
+                Scanner f = new Scanner(new File("datos.txt"));
+                linea = f.nextLine();
+                linea2 = f.nextLine();
+                linea3 = f.nextLine();
+                if (txfNombre.getText().equals("") && txfEdad.getText().equals("") && txfDir.getText().equals("")) {
+                    txfNombre.setText(linea);
+                    txfEdad.setText(linea2);
+                    txfDir.setText(linea3);
+                } else {
+                    int respuesta = JOptionPane.showConfirmDialog(null,
+                            "¿Deseas reemplazar los datos?", "Confirmación",
+                            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if (respuesta == JOptionPane.YES_OPTION) {
+                        txfNombre.setText(linea);
+                    txfEdad.setText(linea2);
+                    txfDir.setText(linea3);
+                    }
+                }
+            } catch (FileNotFoundException excp2) {
+                JOptionPane.showMessageDialog(null, "No existe el archivo", "ERROR!", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+
     }
 }
---
+++ programacion/boletin_swing/src/main/java/principal/Ejercicio4/Principal.java
@@ -0,0 +1,10 @@
+package principal.Ejercicio4;
+
+public class Principal {
+    public static void main(String[] args) {
+        Formulario f = new Formulario();
+
+        f.setSize(350,350);
+        f.setVisible(true);
+    }
+}
