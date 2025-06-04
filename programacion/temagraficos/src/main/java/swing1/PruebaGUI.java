ef44fc4f9184a7a5fbc99a8b7cdf3aa1c0911f13
joeloto <joelme.0808@gmail.com>
2025-06-04 19:09:08 +0200

    g

--- programacion/temagraficos/src/main/java/swing1/PruebaGUI.java
+++ programacion/temagraficos/src/main/java/swing1/PruebaGUI.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public class PruebaGUI {
     public static void main(String[] args) {
-        EventosRaton frame = new EventosRaton();
+        TimerSwing frame = new TimerSwing();
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
